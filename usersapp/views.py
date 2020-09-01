f4a31d6cf2ace0496a2cef37c269f8040bd2d3df
Jihye Lee <zhezhe0206@naver.com>
2020-09-01 23:51:23 +0900

    Add Login

--- usersapp/views.py
+++ usersapp/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def login(request):
-    context={}
-    return render(request,'login.html',context)
+    return render(request,'login.html')
